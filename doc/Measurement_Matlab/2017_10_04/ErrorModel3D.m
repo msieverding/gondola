ad3b48b893b2233dcdf0e61b2303606f5a760015
msieverding <sieverding.marvin@gmx.de>
2017-10-06 14:22:25 +0200

    changed input of error model to a coordinate

--- doc/Measurement_Matlab/2017_10_04/ErrorModel3D.m
+++ doc/Measurement_Matlab/2017_10_04/ErrorModel3D.m
@@ -1,15 +1,22 @@
 clearvars;
 close all;
 
+Coordinate = [400 1500 2000];
+AnchorPos1 = [0; 0; 0];
+AnchorPos2 = [0; 3236; 0];
+AnchorPos3 = [0; 1234; 7697];
+
 dpos = 20;          % error in mm (+)
 dneg = 20;          % error in mm (-)
-r1 = 4000;           % length of rope 1
-r2 = 3000;          % length of rope 2
-r3 = 3000;
+% length of ropes
+r1 = sqrt((AnchorPos1(1) - Coordinate(1))^2 + (AnchorPos1(2) - Coordinate(2))^2 + (AnchorPos1(3) - Coordinate(3))^2);
+r2 = sqrt((AnchorPos2(1) - Coordinate(1))^2 + (AnchorPos2(2) - Coordinate(2))^2 + (AnchorPos2(3) - Coordinate(3))^2);
+r3 = sqrt((AnchorPos3(1) - Coordinate(1))^2 + (AnchorPos3(2) - Coordinate(2))^2 + (AnchorPos3(3) - Coordinate(3))^2);
+
 % only used for formulas
-Anchor1YOffset = 3236;
-Anchor2YOffset = 1234;
-Anchor2ZOffset = 7697;
+Anchor2YOffset = 3236;
+Anchor3YOffset = 1234;
+Anchor3ZOffset = 7697;
 
 % combining the edges is enough
 combinations = [r1+dpos r2+dpos r3+dpos;
@@ -26,16 +33,18 @@ combinations = [r1+dpos r2+dpos r3+dpos;
 % TODO why are there some complex numbers in X?
 y = (combinations(:,1).^2 - combinations(:,2).^2 + 10471696) / 6472;
 z = (combinations(:,2).^2 - combinations(:,3).^2 + 4004 * y + 50294869) / 15394;
-x = real(sqrt(-(combinations(:,3).^2 - (y-1234).^2 - (z-7697).^2)));
+x = sqrt((combinations(:,3).^2 - (y-1234).^2 - (z-7697).^2));
 
 y_real = (r1.^2 - r2.^2 + 10471696) / 6472;
 z_real = (r2.^2 - r3.^2 + 4004 * y_real + 50294869) / 15394;
-x_real = real(sqrt(-(r3.^2 - (y_real-1234).^2 - (z_real-7697).^2)));
+x_real = sqrt((r3.^2 - (y_real-1234).^2 - (z_real-7697).^2));
 
 
 figure;
-plot3(y_real, x_real, z_real, 'gx');
+plot3(Coordinate(2), Coordinate(1), Coordinate(3), 'gx');
 hold on;
+%plot3(y_real, x_real, z_real, 'gx'); position calculated with calculation
+%for the other points. Just to check it
 plot3(y, x, z, 'ro');
 title("3D error with given 1D error");
 grid on;
@@ -75,13 +84,15 @@ r1d = linspace(r1-dneg, r1+dpos, (dneg + dpos + 1)*gridintensity)';
 r2d = linspace(r2-dneg, r2+dpos, (dneg + dpos + 1)*gridintensity)';
 r3d = linspace(r3-dneg, r3+dpos, (dneg + dpos + 1)*gridintensity)';
 [r1d ,r2d ,r3d] = meshgrid(r1d, r2d, r3d);
+
 r1d = r1d(:);
 r2d = r2d(:);
 r3d = r3d(:);
 y_all = (r1d.^2 - r2d.^2 + 10471696) / 6472;
 z_all = (r2d.^2 - r3d.^2 + 4004 * y_all + 50294869) / 15394;
-x_all = sqrt(-(r3d.^2 - (y_all-1234).^2 - (z_all-7697).^2));
+x_all = sqrt((r3d.^2 - (y_all-1234).^2 - (z_all-7697).^2));
 x_all = real(x_all); 
+
 figure;
 plot3(y_real, x_real, z_real, 'gx');
 hold on;
